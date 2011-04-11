76427b69df50d7e3eba016792d693669d0b4effa
Jacob Mather <jmather@itsmajax.com>
2011-04-11 04:24:20 -0400

    Added a bootstrap file to autoload MajaxInstaller for unit tests

---
+++ test/phpunit/bootstrap/MajaxInstaller.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: jmather
+ * Date: 4/11/11
+ * Time: 4:09 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+require_once dirname(__FILE__).'/../../../vendor/MajaxInstaller/MajaxInstaller.php';
+
+MajaxInstaller::autoload();
